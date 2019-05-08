08ff4c8eb346342d20d1df43efa93e3717e4b7f4
Joanne Hooton <jhooton@EM-02342-MBP.local>
2019-05-08 17:19:29 +0100

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,16 +2,15 @@ def my_select(array)
   if block_given?
     i = 0
   else puts "This block should not run!"
-
+  end
     new_array = []
 
     while i < array.length
-      result = yield(array[i])
-      i = i + 1
-      if result == true
-        new_array.push(result)
+      if yield(array[i])
+        new_array.push(array[i])
       end
+      i+=1
     end
     new_array
-  end
+
 end
